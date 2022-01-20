5b70350be55a4439b2047ae068b459ea7c113024
SkipperProg <ambeloslydcev@yandex.ru>
2022-01-20 22:48:14 +0300

    Before global changes

---
+++ src/main/java/model/User.java
@@ -0,0 +1,109 @@
+package model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "Id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "FIRSTNAME")
+    private String firstName;
+
+    @Column(name = "LASTNAME")
+    private String lastName;
+
+    @Column(name = "password")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role"}, name = "user_roles_unique")})
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Set<Role> roles;
+
+    public User() {};
+
+    public User(String email, String firstName, String lastName, String password, Set<Role> roles) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        // this.password = password;
+        this.roles = roles;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && Objects.equals(password, user.password) && Objects.equals(roles, user.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, firstName, lastName, password, roles);
+    }
+}
+
--- src/main/java/ru/skipp/autopartstore/AutoPartStoreApplication.java
+++ src/main/java/ru/skipp/autopartstore/AutoPartStoreApplication.java
@@ -1,14 +1,33 @@
 package ru.skipp.autopartstore;
 
+import lombok.AllArgsConstructor;
+import model.Role;
+import model.User;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import repository.UserRepository;
 
+import java.util.Set;
+
+@ComponentScan("ru.skipp.repositories")
 @SpringBootApplication
-public class AutoPartStoreApplication extends SpringBootServletInitializer {
+@AllArgsConstructor
+public class AutoPartStoreApplication implements ApplicationRunner {
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoPartStoreApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) {
+		userRepository.save(new User("user@gmail.com", "User_First", "User_Last", "password", Set.of(Role.ROLE_USER)));
+		userRepository.save(new User("admin@yandex.ru", "Admin_First", "Admin_Last", "admin", Set.of(Role.ROLE_USER, Role.ROLE_ADMIN)));
+		System.out.println(userRepository.findAll());
 	}
 
 }
